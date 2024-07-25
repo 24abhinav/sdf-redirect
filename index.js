53dcd2a9cbb157d59526b1abb20ea11534406af2
Abhinav Anand <abhinav1813@gmail.com>
2024-07-25 21:51:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  const { url = '' } = req;
+  res.writeHead(301, { location: `satgharadevelopmentfoundation.org${url}` });
+  res.end();
+});
+
+server.listen(3000, () => {
+  console.log('Server is Running');
+});
 
